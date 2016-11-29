aa8676d397133bcee2a74d3768fce293222a7a98
sunny <xiaoyao_xiao@126.com>
2016-11-29 14:52:29 +0800

    更新config文件

--- config/config.php
+++ config/config.php
@@ -8,23 +8,25 @@
  */
 return [
     // 数据库类型
-    'db_type'        => 'mysql',
+    'db_type'           => 'pdo_mysql',
     // 服务器地址
     'hostname'       => '127.0.0.1',
     // 数据库名
-    'database'       => 'test',//'s_admin',
+    'database'       => 's_admin',
     // 用户名
     'username'       => 'root',
     // 密码
     'password'       => '',
     // 端口
     'hostport'       => '3306',
+    // 连接dsn
+    'dsn'            => '',
+    // 数据库连接参数
+    'params'         => [],
     // 数据库编码默认采用utf8
     'charset'        => 'utf8',
     // 数据库表前缀
     'prefix'         => 'darling_',
-    // 数据库调试模式
-    'debug'          => true,
     // 默认全局过滤方法 用逗号分隔多个
     'default_filter' => '',
     //默认模块名
