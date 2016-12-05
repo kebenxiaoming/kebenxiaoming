4f940c06c8cdb0cd530db8380c197e35cb73f220
sunny <xiaoyao_xiao@126.com>
2016-12-05 11:23:12 +0800

    模块管理控制器

--- application/admin/controller/Module.php
+++ application/admin/controller/Module.php
@@ -10,38 +10,36 @@ namespace app\admin\controller;
 
 class Module extends Base{
     public function index(){
+        $count=model("Module")->count();
         $listrows=config("LISTROWS")?config("LISTROWS"):10;
-        $modules=model("Module")->paginate($listrows);
+        $page=new \sunny\Page($count,$listrows);
+        $modules=model("Module")->limit($page->firstRow,$page->listRows)->select();
         $this->assign("modules",$modules);
-        $this->assign("page_html",$modules->render());
+        $this->assign("page_html",$page->show());
         $acjs=renderJsConfirm("icon-remove");
         $this->assign("action_confirm",$acjs);
-        return $this->fetch();
+        $this->display();
     }
     //修改模块内容
     public function edit(){
         if(IS_POST) {
             $module = model("Module");
             $data=input("post.");
-            $res = $module->isUpdate(true)->save($data);
+            $res = $module->update($data);
             if ($res>=0) {
                 Adminlog(session("user")['user_name'],"MODIFY" , "Module", $data['module_id'] ,json_encode($data) );
-                $this->success("修改成功！", url("Module/index"));
-                die;
+                $this->success("修改成功！", url("Module/index"));die;
             } else {
                 $this->error("修改失败！", url("Module/index"));die;
             }
         }else{
             $module_id=intval(input('param.module_id'));
-            $moduleobj=model("Module")->find($module_id);
-            $module=array();
-            if(!empty($moduleobj)){
-                $module=$moduleobj->toArray();
-            }else{
-                $this->error("未获取到该模块！");
+            $module=model("Module")->find($module_id);
+            if(empty($module)){
+                $this->error("未获取到该模块！");die;
             }
             $this->assign("currentmodule",$module);
-            return $this->fetch();
+            $this->display();
         }
     }
 
@@ -59,7 +57,7 @@ class Module extends Base{
                 die;
             }
         }else{
-            return $this->fetch();
+            $this->display();
         }
     }
 
@@ -73,9 +71,8 @@ class Module extends Base{
             }
             $res = model("Module")->where($data)->delete();
             if ($res) {
-                Adminlog(session("user")['user_name'],"DEL" , "Module",$data['module_id'] ,json_encode($moduleobj->toArray()) );
-                $this->success("删除成功！", url("Module/index"));
-                die;
+                Adminlog(session("user")['user_name'],"DEL" , "Module",$data['module_id'] ,json_encode($moduleobj) );
+                $this->success("删除成功！", url("Module/index"));die;
             } else {
                 $this->error("删除失败！", url("Module/index"));die;
             }
