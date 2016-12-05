ca146a8bb497a1314b50a8b8148489c5c9fb3f99
sunny <xiaoyao_xiao@126.com>
2016-12-05 13:37:54 +0800

    修改后台页面中的“首页”链接

--- application/admin/view/Public/navibar.php
+++ application/admin/view/Public/navibar.php
@@ -2,8 +2,8 @@
 <div id="header">
     <table width="1200" border="0" cellspacing="0" cellpadding="0" class="header_nr">
         <tr>
-            <td height="69"><img src="<?php echo config('PUBLIC');?>/Admin/images/sy_03.jpg" width="53" height="23" /><a href="/public/admin">首页</a></td>
-            <?php if(!empty($user_info)){?><td align="right">欢迎，<a href="<?php echo url('User/edit',array('user_id'=>$user_info['user_id']));?>"><?php echo $user_info['user_name']?></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo url('Login/logout');?>">退出</a></td><?php } ?>
+            <td height="69"><img src="<?php echo config('PUBLIC');?>/Admin/images/sy_03.jpg" width="53" height="23" /><a href="<?php echo url('Index/index');?>">首页</a></td>
+            <?php if(!empty($this->vars['user_info'])){?><td align="right">欢迎，<a href="<?php echo url('User/edit',array('user_id'=>$this->vars['user_info']['user_id']));?>"><?php echo $this->vars['user_info']['user_name']?></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo url('Login/logout');?>">退出</a></td><?php } ?>
         </tr>
     </table>
 </div>
