46eaa9af5363aba1b607b973763c3c69a55edf58
sunny <xiaoyao_xiao@126.com>
2016-12-05 13:54:25 +0800

    新增测试文件上传的页面

---
+++ application/admin/view/File/upload.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<form method="post" enctype="multipart/form-data">
+    <input type="file" name="image"/>
+    <input type="submit" name="提交"/>
+</form>
+</body>
+</html>
