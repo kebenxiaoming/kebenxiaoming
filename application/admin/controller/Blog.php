df779f7732f787c592bce6eb1fb9c8d3ebd84d2c
sunny <xiaoyao_xiao@126.com>
2016-12-06 14:28:26 +0800

    修改文件上传适应博客的编辑

--- application/admin/controller/Blog.php
+++ application/admin/controller/Blog.php
@@ -21,4 +21,79 @@ class Blog extends Base{
         $this->assign("action_confirm",$acjs);
         $this->display();
     }
+
+    //新增
+    public function add(){
+        if(IS_POST) {
+            $data=input('post.');
+            if (!empty($data)) {
+                if(empty($data['sort'])){
+                    $data['sort']=0;
+                }
+                $data['status']=1;
+                $now=time();
+                $data['create_time']=$now;
+                $data['update_time']=$now;
+                if ($res = model("Article")->save($data)) {
+                    $this->success("新增文章成功！",url('Blog/index'));
+                    die;
+                } else {
+                    $this->error("新增文章失败！",url('Blog/add'));
+                    die;
+                }
+            } else {
+                $this->error("获取数据失败！",url('Blog/add'));
+                die;
+            }
+        }
+        $this->display();
+    }
+    //编辑博客
+    public function edit(){
+        if(IS_POST) {
+            $data=input('post.');
+            if (!empty($data)) {
+                if(empty($data['sort'])){
+                    $data['sort']=0;
+                }
+                $now=time();
+                $data['update_time']=$now;
+                if ($res = model("Article")->update($data)) {
+                    $this->success("编辑文章成功！",url('Blog/index'));
+                    die;
+                } else {
+                    $this->error("编辑文章失败！",url('Blog/index'));
+                    die;
+                }
+            } else {
+                $this->error("获取数据失败！",url('Blog/index'));
+                die;
+            }
+        }else{
+            $id=input("get.id");
+            $news=model("Article")->find($id);
+            if(!empty($news)){
+                //图片
+                if(!empty($news['pics'])){
+                    $where="id IN (".$news['pics'].")";
+                    $files = model("File")->where($where)->select();
+                    $this->assign("pics", $files);
+                }
+                $this->assign("article",$news);
+            }else{
+                $this->error("未获取到文章详情！");die;
+            }
+        }
+        $this->display();
+    }
+
+    //删除
+    public function del(){
+        $id=input('get.id');
+        if($res=model("Article")->where("id=".$id)->delete()){
+            $this->success("删除成功!",url('Blog/index'));die;
+        }else{
+            $this->error("删除失败！",url('Blog/index'));die;
+        }
+    }
 }
--- application/admin/controller/File.php
+++ application/admin/controller/File.php
@@ -9,11 +9,37 @@
 namespace app\admin\controller;
 
 class File extends Base{
-    public function upload(){
-        if(IS_POST) {
-            $info=model("File")->upload();
-            print_r($info);die;
+    public function uploadEdit(){
+
+        $return  = array('error' => 0, 'url' => '');
+        /* 调用文件上传组件上传文件 */
+        $info=model("File")->upload("imgFile");
+        /* 记录附件信息 */
+        if($info){
+            $return['url'] = "/uploads".DS.$info['info']['savepath'];
+        } else {
+            $return['error']=1;
         }
-        $this->display();
+        /* 返回JSON数据 */
+        $this->ajaxReturn($return);
+    }
+
+    //编辑器上传文件
+    public function uploadPicture(){
+        $return  = array('status' => 1, 'info' => '上传成功', 'data' => '');
+        /* 调用文件上传组件上传文件 */
+        $info=model("File")->upload("Filedata");
+        /* 记录附件信息 */
+        if($info){
+            $info['info']['ossres']="/uploads".DS.$info['info']['savepath'];
+            $return['data'] = $info['info'];
+            $return['info'] = "上传成功！";
+        } else {
+            $return['status'] = 0;
+            $return['info']   = $info['info'];
+        }
+
+        /* 返回JSON数据 */
+        $this->ajaxReturn($return);
     }
 }
