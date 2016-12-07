37ce438505e835f3bd939fa13cded832ef8349ae
sunny <xiaoyao_xiao@126.com>
2016-12-07 09:57:30 +0800

    修改页面样式

--- application/home/view/Blog/show.php
+++ application/home/view/Blog/show.php
@@ -3,26 +3,14 @@ require dirname(dirname(__FILE__))."/Public/header.php";
 ?>
     <div id="fh5co-about-section">
         <div class="container">
-            <div class="row">
                 <div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
                     <h2><?php echo $this->vars['blog']['title'];?></h2>
                     <p><?php echo $this->vars['blog']['description'];?></p>
                 </div>
+            <div class="col-md-8 sunny" style="margin:0 auto;width: 100%;">
+                    <?php echo $this->vars['blog']['content'];?>
             </div>
-            <div class="row">
-                <div class="col-md-8" style="width: 100%;">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="about-inner">
-                                <?php echo $this->vars['blog']['content'];?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
-
 <?php
 require dirname(dirname(__FILE__))."/Public/footer.php";
 ?>
