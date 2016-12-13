aaed6c0e28b3d76adc34c14ecf36911855755a09
sunny <xiaoyao_xiao@126.com>
2016-12-13 09:34:44 +0800

    略微修改了一点问题

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,11 +1,10 @@
 <?php
 namespace app\admin\controller;
 
-
 class Index extends Base
 {
     public function index(){
         // 模板输出
-        return $this->display();
+        $this->display();
     }
 }
